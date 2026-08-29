39af6fe4e2c68f198776b660e3ed6194d50bc43d
Omkar <omkarkale2204@gmail.com>
2026-08-30 00:09:12 +0530

    Transactions models setup

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import engine_from_config, pool
 from app.core.config import get_settings
 from app.core.database import Base
 from app.modules.accounts.models import Account
+from app.modules.transactions.models import Transaction
 
 config = context.config
 
